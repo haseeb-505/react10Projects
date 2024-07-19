d2d73ef3ababa224add256da2a2c9e094398ed10
Haseeb Ahmad Tahir <hatahir87626@gmail.com>
2024-07-19 20:15:16 +0500

    initial commit

--- travel_suggestion/vite.config.js
+++ travel_suggestion/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/react10Projects",
   plugins: [react()],
-  base:"/travel_suggestion/travel_suggestion/",
 })
